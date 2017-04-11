e1bedf4a57718aff95c2bec4b3576957ea77970e
Kevin Gallagher <kevingallagher@gmail.com>
2017-04-11 16:49:16 -0700

    Add Jinja2 timestamp filter

--- showblocks.py
+++ showblocks.py
@@ -3,7 +3,7 @@
 
 from functools import wraps
 from flask import Flask, g, abort, jsonify, render_template, request, Response, session
-import config, json, os, sqlite3, sys
+import config, json, os, sqlite3, sys, time
 
 db_file = 'blocks.sqlite'
 app = Flask(__name__)
@@ -15,6 +15,10 @@ def get_blocks():
     c.execute('SELECT * FROM blocks')
     blocks = c.fetchall()
     return blocks
+
+@app.template_filter('timestamp')
+def _jinja2_filter_timestamp(unix_epoch):
+    return time.ctime(unix_epoch)
 
 @app.route('/')
 def index():
