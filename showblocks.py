858cd737a345dc84188cda79e4da1bec1605a4d8
Jay Graber <arcalinea@users.noreply.github.com>
2017-04-24 19:42:34 -0700

    Sorting by column, fix search by tx (#2)
    * gitignore .pyc files and venv dir
    * Improve UI with bootstrap
    * Add footer, pagination by height, slight refactoring, disable cache
    * Fix search by tx
    * Add dynamic sorting with DataTables, reorg static folder
    * Add bootstrap for datatables
    * Use datatables pagination -- will have to figure out lazy loading for records, capped at 200 right now

--- showblocks.py
+++ showblocks.py
@@ -10,7 +10,7 @@ db_file = 'blocks.sqlite'
 app = Flask(__name__)
 app.config.from_object(config.FlaskConfig)
 
-def find_block_by_tx_by_tx(txid):
+def find_block_by_tx(txid):
     conn = sqlite3.connect(db_file)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
@@ -28,19 +28,19 @@ def get_single_block(block_hash):
     block['tx'] = txs
     return block
 
-
-def get_blocks(query):
+def get_blocks(query={}):
     print "in get blocks"
     if query.get('height'):
         height = query['height']
     conn = sqlite3.connect(db_file)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    if height == 'top':
-        c.execute('SELECT hash, height, size, time FROM blocks ORDER by height DESC LIMIT 20')
-    else:
-        bottom = height - 20
-        c.execute('SELECT hash, height, size, time FROM blocks WHERE height<=:top AND height>:bottom ORDER by height DESC', {"top":height, "bottom":bottom})
+    # if height == 'top':
+    #     c.execute('SELECT hash, height, size, time FROM blocks ORDER by height DESC LIMIT 20')
+    # else:
+    #     bottom = height - 20
+    #     c.execute('SELECT hash, height, size, time FROM blocks WHERE height<=:top AND height>:bottom ORDER by height DESC', {"top":height, "bottom":bottom})
+    c.execute('SELECT hash, height, size, time FROM blocks ORDER by height DESC LIMIT 200')
     # return retrieved blocks as a dict, with transactions
     blocks = [dict(block) for block in c.fetchall()]
     for block in blocks:
@@ -69,8 +69,9 @@ def validate_input(search_string):
 def _jinja2_filter_timestamp(unix_epoch):
     return time.ctime(unix_epoch)
 
-@app.route('/', defaults={'height': 'top'})
-@app.route('/height/<int:height>')
+# @app.route('/', defaults={'height': 'top'})
+# @app.route('/height/<int:height>')
+@app.route('/')
 def index(height='top'):
     query = {"height":height}
     try:
@@ -83,12 +84,12 @@ def index(height='top'):
 @app.route('/block', methods = ['GET', 'POST'])
 def show_block():
     print 'search: ', request.values.get('search')
-    block_hash_search = validate_input(request.values.get('search').strip().lower())
-    if block_hash_search is None:
+    search_string = validate_input(request.values.get('search').strip().lower())
+    if search_string is None:
         print 'blockhash search none'
         return ('', 204)
     try:
-        block = get_single_block(block_hash_search)
+        block = get_single_block(search_string)
         return render_template('block.html', block = block)
     except:
         pass
@@ -102,5 +103,5 @@ def show_block():
 
 if __name__ == '__main__':
     cache = SimpleCache()
-    cache.set('blocks', get_blocks({"height":'top'}), timeout=3600)
+    cache.set('blocks', get_blocks(), timeout=3600)
     app.run(host='0.0.0.0', port=int('8201'), debug=True)
--- static/js/site.js
+++ static/js/site.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  $('#recentBlocks').DataTable({
+    "info": false,
+    "order": [[ 0, "desc" ]],
+    "oLanguage": {
+      "sSearch": "Filter blocks on page by search criteria:"
+    }
+  });
+
   $('#moreBlocks').click(function(e){
     e.preventDefault()
     last = $('#recentBlocks tr:last-child td:first-child').html()
