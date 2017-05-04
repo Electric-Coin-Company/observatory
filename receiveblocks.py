e42e9a34da36ae603550d4c1921332488d019713
Kevin Gallagher <kevingallagher@gmail.com>
2017-05-04 06:10:30 -0700

    Remove unused imports in receiveblocks.py

--- receiveblocks.py
+++ receiveblocks.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from functools import wraps
-from flask import Flask, abort, jsonify, request, Response, session
-import config, json, os, sqlite3, sys
+from flask import Flask, request
+import config, json, sqlite3
 
 db_file = 'blocks.sqlite'
 app = Flask(__name__)
@@ -62,13 +61,13 @@ def storeblock(block):
             try:
                 c.execute('UPDATE blocks SET nextblockhash=:nextblockhash WHERE hash=:hash',
                     {"nextblockhash": block['nextblockhash'], "hash": block['hash']})
-                print 'Updated nextblockhash on block ' + block['height']
+                print('Updated nextblockhash on block ' + block['height'])
             except:
                 pass
         try:
             c.execute('UPDATE blocks SET confirmations=:confirmations WHERE hash=:hash',
                 {"confirmations": block['confirmations'], "hash": block['hash']})
-            print 'Updated confirmations on block ' + block['height']
+            print('Updated confirmations on block ' + block['height'])
         except:
             pass
     for tx in block['tx']:
@@ -81,7 +80,7 @@ def storeblock(block):
 
 @app.route('/', methods = ['POST'])
 def index():
-    print request.get_data()
+    print(request.get_data())
     if request.method == 'POST' and request.content_type == 'application/json':
         storeblock(request.get_json())
         return ('', 204)
