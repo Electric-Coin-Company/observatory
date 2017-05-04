2516899129a5d133e33db8f2e4946be792e48857
Kevin Gallagher <kevingallagher@gmail.com>
2017-05-04 15:24:09 -0700

    Allow command-line arguments for loadblocks.py

--- loadblocks.py
+++ loadblocks.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
 import json
+import os
 import subprocess
 import requests
 
 from config import LoadBlocksConfig
 config = LoadBlocksConfig
+
+def parse_cmd_args():
+    description = """
+Allows one to specify the block range to import via STARTING_BLOCK_HEIGHT and ENDING_BLOCK_HEIGHT
+Examples:
+     export STARTING_BLOCK_HEIGHT=1234
+     export ENDING_BLOCK_HEIGHT=12345678
+     ./loadblocks.py
+"""
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--start', type=int, default=os.environ.get('STARTING_BLOCK_HEIGHT', 0), required=False, help="Block number to begin import from")
+    parser.add_argument('--end', type=int, default=os.environ.get('ENDING_BLOCK_HEIGHT', None), required=False, help="Block number to stop importing at")
+    return parser.parse_args()
 
 def zcash(cmd):
     zcash = subprocess.Popen([config.ZCASH_CLI_PATH, cmd], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -31,14 +47,19 @@ def get_block(block_height):
     return json.loads(output)
 
 def main():
+    args = parse_cmd_args()
+    is_zcashd_running()
+    if not (args.start or args.end):
+        start_point = config.STARTING_BLOCK_HEIGHT
+        end_point = config.ENDING_BLOCK_HEIGHT
     number_of_blocks = zcash('getinfo')['blocks'] if zcash('getinfo') is not False else None
     if number_of_blocks is not None:
         session = requests.session()
         session.headers.update({'Content-Type': 'application/json'})
-        for x in range(config.STARTING_BLOCK_HEIGHT if (config.STARTING_BLOCK_HEIGHT > 0) \
-            else config.STARTING_BLOCK_HEIGHT, config.ENDING_BLOCK_HEIGHT \
-            if (config.ENDING_BLOCK_HEIGHT < number_of_blocks) \
-            else config.ENDING_BLOCK_HEIGHT):
+        for x in range(start_point if (start_point > 0) \
+            else start_point, end_point \
+            if (end_point < number_of_blocks) \
+            else end_point):
                 block = get_block(str(x))
                 r = session.post(config.BLOCK_OBSERVATORY_URL, json=block)
                 r.raise_for_status()
