0af04340b59a72cf41473d95326d2e5934047162
Kevin Gallagher <kevingallagher@gmail.com>
2017-05-04 06:06:56 -0700

    Auto-reload templates to suppress caching

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ class FlaskConfig(object):
   DEBUG = True
   TESTING = True
   TEMPLATE_DEBUG = True
+  TEMPLATES_AUTO_RELOAD = True
   WTF_CSRF_ENABLED = False
   SECRET_KEY = 'lQT0EE/XcyZrXDjCCJ/KRs3F2zKc0Ls3KAmT4y0pxp4='
