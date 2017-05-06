4f578895f0570ed2b2190cb592c2abaab75e58da
Kevin Gallagher <kevingallagher@gmail.com>
2017-05-05 22:14:06 -0700

    PEP8 formatting stuff, also preferring Python3

--- sendblock.py
+++ sendblock.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Sends incoming blocks from a locally-running zcashd to a remote collector.
@@ -12,10 +12,13 @@ import requests
 from config import SendBlocksConfig
 config = SendBlocksConfig
 
+
 def zcash(block_hash):
-    zcexec = subprocess.Popen([config.ZCASH_CLI_PATH, 'getblock', block_hash], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    zcexec = subprocess.Popen([config.ZCASH_CLI_PATH, 'getblock', block_hash],
+                              stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     output = zcexec.communicate()[0]
     return json.loads(output)
+
 
 def main():
     timestamp = int(time.time())
@@ -27,6 +30,7 @@ def main():
     r = session.post(config['BLOCK_OBSERVATORY_URL'], json=block)
     r.raise_for_status()
     sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
