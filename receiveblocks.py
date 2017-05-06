91ae4ccb1caa889e6244f14b859ed5e59e3acf84
Kevin Gallagher <kevingallagher@gmail.com>
2017-05-05 22:29:58 -0700

    Identify missing ranges of blocks and fill them in

--- receiveblocks.py
+++ receiveblocks.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 This is a Python Flask web application for storing Zcash blocks from elsewhere.
 """
 import json
 import sqlite3
+import subprocess
+import sys
 from flask import Flask, request
 
 import config
@@ -43,6 +45,42 @@ def createdb():
     conn.execute('VACUUM')
     conn.commit()
     conn.close()
+
+def find_gaps():
+    conn = sqlite3.connect(app.config['DB_FILE'])
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute('SELECT (t1.height + 1) AS start, \
+        (SELECT MIN(t3.height) -1 FROM blocks t3 WHERE t3.height > t1.height) AS end \
+        FROM blocks t1 \
+        WHERE NOT EXISTS (SELECT t2.height FROM blocks t2 WHERE t2.height = t1.height + 1) \
+        GROUP BY height HAVING end IS NOT NULL \
+        ORDER BY end DESC')
+    gaps = [dict(gap) for gap in c.fetchall()]
+    if gaps == []:
+        return None
+    else:
+        for gap in gaps:
+            print('Blocks between %d and %d are absent.' % (gap['start'], gap['end']))
+        return gaps
+
+def fill_gaps(gaps):
+    commands = []
+    procs = []
+    exit_statuses = []
+    for gap in gaps:
+        commands.append(['/usr/bin/python', './loadblocks.py', '--start ' + str(gap['start']), '--end ' + str(gap['end'])])
+    procs = [subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True) for cmd in commands]
+    for p in procs:
+        print('Running %s as pid %d' % (p.args[1], p.pid))
+    for p in procs:
+        while p.poll() is None:
+            sys.stdout.write(p.stdout.readline())
+            sys.stdout.flush()
+            p.kill()
+    exit_statuses = [p.wait() for p in procs]
+    print(exit_statuses)
+    return True
 
 def storeblock(block):
     conn = sqlite3.connect(app.config['DB_FILE'])
@@ -90,4 +128,5 @@ def index():
 
 if __name__ == '__main__':
     createdb()
+    fill_gaps(find_gaps())
     app.run(host='0.0.0.0', port=int(app.config['BIND_PORT']))
